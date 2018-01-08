ba0364e2ce00565c3573f14e91582469a7dad8cc
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-01-08 16:33:12 +1000

    use new and improved acme method
    use unix time for DNSCypt cert key ID
    Add addtional IPs (peer_ip) to the DNS pool

--- roles/dnsdist-post/templates/dnscrypt.lua
+++ roles/dnsdist-post/templates/dnscrypt.lua
@@ -14,7 +14,8 @@ then
 end
 
 --Generate init Cert (Expires in 24 hours!)
-generateDNSCryptCertificate("/etc/dnsdist/providerPrivate.key", "/etc/dnsdist/resolver.cert.0", "/etc/dnsdist/resolver.key.0", 0, os.time() - 60 , os.time() + 86340)
+last = os.time()
+generateDNSCryptCertificate("/etc/dnsdist/providerPrivate.key", "/etc/dnsdist/resolver.cert.0", "/etc/dnsdist/resolver.key.0", last, last - 60 , last + 86340)
 
 {%- if ansible_default_ipv4.address is defined %}
 {%- set ip = ansible_default_ipv4.address %}
@@ -38,24 +39,22 @@ addDNSCryptBind("{{ip}}:8080", "{{dnscrypt_provider_name}}", "/etc/dnsdist/resol
 addDNSCryptBind("[{{ip6}}]:8080", "{{dnscrypt_provider_name}}", "/etc/dnsdist/resolver.cert.0", "/etc/dnsdist/resolver.key.0")
 -- addDNSCryptBind("{{ip}}:27015", "{{dnscrypt_provider_name}}", "/etc/dnsdist/resolver.cert.0", "/etc/dnsdist/resolver.key.0")
 
-local last = os.time()
-serial = 0
 function autoRenewCert() -- called approx. every second in maintenance function
 
-  -- (60*60*23) - 300 = 82500 (23 hours)  - 1 hour for clients to update cirt + clock diff of 5 min
+  -- (60*60*23) - 300 = 82500 (23 hours)  - 1 hour for clients to update cert + clock diff of 5 min
   -- 60*60*24 - 60 = 86340 (24 hours) -- cert expires in 24 - clock diff of 1 min
   local now = os.time()
-  if ((now - last) > 82500) then
-    serial = serial + 1
+  local diff = now - last
+  if (diff > 82500) then
 
-    getDNSCryptBind(0):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", serial, now - 60, now + 86340)
-    getDNSCryptBind(1):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", serial, now - 60, now + 86340)
-    getDNSCryptBind(2):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", serial, now - 60, now + 86340)
-    getDNSCryptBind(3):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", serial, now - 60, now + 86340)
-    getDNSCryptBind(4):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", serial, now - 60, now + 86340)
-    getDNSCryptBind(5):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", serial, now - 60, now + 86340)
-    -- getDNSCryptBind(6):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", serial, now - 60, now + 86340)
-    -- getDNSCryptBind(7):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", serial, now - 60, now + 86340)
+    getDNSCryptBind(0):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", now, now - 60, now + 86340)
+    getDNSCryptBind(1):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", now, now - 60, now + 86340)
+    getDNSCryptBind(2):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", now, now - 60, now + 86340)
+    getDNSCryptBind(3):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", now, now - 60, now + 86340)
+    getDNSCryptBind(4):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", now, now - 60, now + 86340)
+    getDNSCryptBind(5):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", now, now - 60, now + 86340)
+    -- getDNSCryptBind(6):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", now, now - 60, now + 86340)
+    -- getDNSCryptBind(7):generateAndLoadInMemoryCertificate("/etc/dnsdist/providerPrivate.key", now, now - 60, now + 86340)
     last = now
   end
 end
